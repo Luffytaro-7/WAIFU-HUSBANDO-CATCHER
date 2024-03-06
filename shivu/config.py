c60d7976425d44aa82279fd23e1a1a3d36898aea
Monkey D. Luffy <160203957+Luffytaro-7@users.noreply.github.com>
2024-03-06 23:07:00 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -3,16 +3,15 @@ class Config(object):
 
     # Get this value from my.telegram.org/apps
     OWNER_ID = "6805772957"
-    sudo_users = "6805772957", "851088427"
-    GROUP_ID = -1002131909973
+    sudo_users = "6805772957","851088427"
+    GROUP_ID = "-1002131909973"
     TOKEN = "7030986326:AAHp6WDyj3gkHEQl07XDV1kyrwNppwTcn-E"
-    mongo_url = ""
     PHOTO_URL = ["https://graph.org/bot-pic-03-06", ""]
     SUPPORT_CHAT = "W.C_Support"
     UPDATE_CHAT = "W.c_Support"
     BOT_USERNAME = "makewaifucollectionbot"
     CHARA_CHANNEL_ID = "-1002131909973"
-    api_id = 23913652
+    api_id = "23913652"
     api_hash = "99383e8046e0d7696aa8d73a504486de"
 
     
